73e51bf3d6480ad3dd37664f102c8e975484a6a8
Ryan Jeon <yejeon2020@u.northwestern.edu>
2017-10-23 13:27:21 -0500

    -Edited functionality of Reset button (resets time) -Fixed start button

--- app/src/main/java/com/example/ryanj/hiit_v1/MainActivity.java
+++ app/src/main/java/com/example/ryanj/hiit_v1/MainActivity.java
@@ -17,11 +17,14 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button stop, start, lap; //button objects
+    Button stop, start, reset; //btton objects
     int set = 0; //will count sets of running
 
     TextView txt, laptxt; //counter text
     long store = 0; //will store time
+
+    Boolean p = false; //default check
+    long counter_time = 60000; //default counter time start 60 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Pausing..", Toast.LENGTH_SHORT).show();
 
+                p = true;
+                txt.setText(String.valueOf(store / 1000));
+                counter_time = store; //set counter time to store
 
             }
         });
@@ -46,9 +52,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Starting the counting", Toast.LENGTH_SHORT).show();
 
+                p = false; //makes the code run again
 
-                new CountDownTimer(60000, 1000) {
+                new CountDownTimer(counter_time, 1000) {
                     public void onTick(long time) {
+                        if(p){
+                            cancel(); //pauses count down
+                        }
+
+
                         store = time; //stores the current time
                         txt.setText(String.valueOf(time / 1000 ));
 
@@ -60,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(getApplicationContext(), "Sprint!", Toast.LENGTH_SHORT).show();
 
                         }
+
 
                     }
 
@@ -73,17 +86,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        lap = (Button) findViewById(R.id.Lap);
-        lap.setOnClickListener(new View.OnClickListener() {
+        reset = (Button) findViewById(R.id.Reset);
+        reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Starting the counting", Toast.LENGTH_SHORT).show();
-                while (store != 10){
-                    store++;
-                    laptxt.setText("fun");
-                    SystemClock.sleep(1000);
+                Toast.makeText(getApplicationContext(), "Resetting the counter..", Toast.LENGTH_SHORT).show();
+                p = true; //pauses the counter
+                store = 0; //reset the stored time
+                counter_time = 60000; //reset the counter time to 60
+                txt.setText(String.valueOf(store)); //reset the value of counter
 
-                }
+
             }
         });
 
